d178bf336648ba05710b2d34d1102e35881dfa19
hidriali <159208504+hidriali@users.noreply.github.com>
2024-03-12 17:46:52 +0000

    AD

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 # Output:       Valid date is printed or user is alerted that an invalid date was entered.
 
 validDate = True
+invalidDate = False
 MIN_YEAR = 0
 MIN_MONTH = 1
 MAX_MONTH = 12
 MIN_DAY = 1
-MAX_DAY = 31
+MAX_DAY = 30
 
-year = None
-month = None
-day = None
+year = input( "Enter year: " )
+month =input( "Enter month: " )
+day = input( "Enter day: " )
 
 # Get the year, then the month, then the day
 # housekeeping()
@@ -20,15 +21,21 @@ day = None
 
 if int(year) <= MIN_YEAR: # invalid year
     validDate = False
-elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
-    validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
-    validDate = False
+  
+elif int(month) < MIN_MONTH or int(month) > MAX_MONTH:
+  validDate = False
+  
+if int(day) < MIN_DAY or int(day) > MAX_DAY:
+  validDate = False  
+
 
 # Test to see if date is valid and output date and whether it is valid or not
 
 # endOfJob()
-if validDate == True:
+if validDate ==True:
+  print(day + "/" + month + "/" + year + " is a valid date.")
     # Output statement
 else:
-    # Output statement
+  #output statement
+  print(day + "/" + month + "/" + year + " is an invalid date.")
+  
